359196e39fb2563152654e9f240edc5e695a0dcd
chgiahuy2709 <chgiahuy2709@gmail.com>
2022-03-01 17:22:35 +0700

    add files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # nft-marketplace
 This is project for NFT with NextJS, using Rinkeby test net deployment 
+git clone https://github.com/ha2709/nft-marketplace.git
---
+++ config.js
@@ -0,0 +1,4 @@
+
+  export const nftmarketaddress = "0xBd2D530281852448Cf6226946D434C04113b2Cc9"
+  export const nftaddress = "0x6E51c26B6DC9f920ce46BEc7271525bEf7BDdbFE"
+  
